d373cbcd4f3a2080c5a9c52b7cc404c0f961a169
Tharollo Kekana <tkekanajnr@gmail.com>
2026-03-19 08:11:02 +0200

    Started with the LogEntriesController

---
+++ HabitTracker/Controllers/LogEntriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using HabitTracker.Data;
+using HabitTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HabitTracker.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LogEntriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public LogEntriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
