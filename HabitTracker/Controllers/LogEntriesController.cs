8c980f48fd64d58ce1cc86540eb6ac06147a67e1
Tharollo Kekana <tkekanajnr@gmail.com>
2026-03-19 09:11:33 +0200

    Added endpoints to the LogEntries Controller

--- HabitTracker/Controllers/LogEntriesController.cs
+++ HabitTracker/Controllers/LogEntriesController.cs
@@ -16,5 +16,46 @@ namespace HabitTracker.Controllers
         {
             _context = context;
         }
+
+        //GET /api/habits/{habitId}/logs
+        [HttpGet("/api/habits/{habitId}/logs")]
+        public async Task<IActionResult> GetAllLogs(int id)
+        {
+            var habit = await _context.LogEntries.FindAsync(id);
+            if (habit == null) 
+                return NotFound(new {message = $"Habit with Id {id} not found"});
+
+            var logs = await _context.LogEntries
+                .Where(l => l.Id == id)
+                .OrderByDescending(l => l.Completed)
+                .ToListAsync();
+
+            return Ok(logs);
+        }
+
+        //Post - /api/habits/{habitId}/logs
+        [HttpPost("/api/habits/{habitId}/logs")]
+        public async Task<IActionResult> LogCompletion(int id)
+        {
+            var habit = await _context.LogEntries.FindAsync(id);
+            if (habit == null)
+                return NotFound(new { message = $"Habit with id {id} not found" });
+
+            var today = DateTime.Now;
+            var alreadyLog = await _context.LogEntries.AnyAsync(l => l.HabitId == id && l.Completed == today);
+
+            if (alreadyLog)
+                return BadRequest(new { message = " This habit has already been logged today." });
+
+            var logEntry = new LogEntry
+            {
+                HabitId = id,
+                Completed = today,
+            };
+
+            _context.LogEntries.Add(logEntry);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAllLogs), new { id }, logEntry);
+        }
     }
 }
